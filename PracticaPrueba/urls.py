007b936b6a8336827829aeff7c6faa964f31497c
jorge <jorgecarmona775@gmail.com>
2022-09-12 14:18:33 -0300

    commit app1 2 views creadas

--- PracticaPrueba/urls.py
+++ PracticaPrueba/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1 import views as app1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('1view1app/',app1.display1),
+    path('2view1app/',app1.display2),
 ]
